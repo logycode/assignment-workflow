978dbb07d0daa6153263d7dd064bb870bff0559c
Tim Kranich <tim@logyco.de>
2022-11-21 12:32:01 +0100

    set default values for user

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: {},
+    user: {
+      firstName: "",
+      lastName: "",
+      githubUserName: "",
+      email: "",
+    },
   },
   mutations: {
     SET_USER_DATA(state, user) {
